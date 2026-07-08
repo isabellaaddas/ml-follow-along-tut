54a616283c5ca63873f331848cfbcf75db77659a
isabellaaddas <isabellaaddas@gmail.com>
2026-07-08 19:03:04 -0400

    added first import statements for first step

--- regression-from-scratch.py
+++ regression-from-scratch.py
@@ -3,3 +3,8 @@
 # Playlist: Machine Learning with Python
 # Regression (from scratch) Section (videos 8-)
 ###
+
+from statistics import mean
+import numpy as np
+
+# Translate the formula for finding slope (m)
