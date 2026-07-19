a21ae47024ca7880577ab47c4c56999b76fa2240
isabellaaddas <isabellaaddas@gmail.com>
2026-07-18 21:00:38 -0400

    added data files to folder for k-nearest neighbors project from UCI ML Repository and set up first import statements in new python file

--- k-nearest-neighbor.py
+++ k-nearest-neighbor.py
@@ -3,3 +3,7 @@
 # Playlist: Machine Learning with Python
 # K-Nearest Neighbor Section (videos 14-)
 ###
+
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+import pandas as pd
