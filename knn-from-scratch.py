d7f2fd121dcbbdf89e6073f0fc5424b317c4f5ca
isabellaaddas <isabellaaddas@gmail.com>
2026-07-19 18:44:53 -0400

    added print statement for distance

--- knn-from-scratch.py
+++ knn-from-scratch.py
@@ -15,3 +15,5 @@ plot2 = [2,5]
 # Euclidean distance calculation on example points
 euclidean_distance = sqrt((plot1[0] - plot2[0])**2 +
                           (plot1[1] - plot2[1])**2)
+
+#print(euclidean_distance)
