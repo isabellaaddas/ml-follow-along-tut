b29556601d41d487d5d9e6432a39a62958396f49
isabellaaddas <isabellaaddas@gmail.com>
2026-07-19 18:42:59 -0400

    added example plot points and euclidean distance operation

--- knn-from-scratch.py
+++ knn-from-scratch.py
@@ -3,3 +3,15 @@
 # Playlist: Machine Learning with Python
 # K-Nearest Neighbor Section (from scratch) (videos 16-)
 ###
+
+from math import sqrt
+
+# Instantiate plot points for demonstrating euclidean
+# distance
+plot1 = [1,3]
+plot2 = [2,5]
+
+# Utilize the sqrt method from math module to perform
+# Euclidean distance calculation on example points
+euclidean_distance = sqrt((plot1[0] - plot2[0])**2 +
+                          (plot1[1] - plot2[1])**2)
