789d98fc984b5851d305e7ee9de125f7914e9942
isabellaaddas <isabellaaddas@gmail.com>
2026-07-18 21:02:49 -0400

    loaded in data as pandas dataframe and replaced missing data with some number

--- k-nearest-neighbor.py
+++ k-nearest-neighbor.py
@@ -7,3 +7,9 @@
 import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
+
+# Read dataframe into program from file
+df = pd.read_csv('./k-nearest-files/breast-cancer-wisconsin.data')
+
+# Replace missing data, denoted by "?", as some tangible #
+df.replace('?', -99999, inplace=True)
