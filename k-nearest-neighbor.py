2a47db8f1a7e841ee157044a4a7487badad9690f
isabellaaddas <isabellaaddas@gmail.com>
2026-08-04 11:53:50 -0400

    ran code 25 times, same as other file, to test average accuracies between the two

--- k-nearest-neighbor.py
+++ k-nearest-neighbor.py
@@ -9,32 +9,37 @@ from sklearn import preprocessing, neighbors
 from sklearn.model_selection import cross_validate, train_test_split
 import pandas as pd
 
-# Read dataframe into program from file
-df = pd.read_csv('./k-nearest-files/breast-cancer-wisconsin.data')
+accuracies = []
+for i in range(25):
+    # Read dataframe into program from file
+    df = pd.read_csv('./k-nearest-files/breast-cancer-wisconsin.data')
 
-# Replace missing data, denoted by "?", as some tangible #
-# (recognized as outlier instead)
-df.replace('?', -99999, inplace=True)
+    # Replace missing data, denoted by "?", as some tangible #
+    # (recognized as outlier instead)
+    df.replace('?', -99999, inplace=True)
 
-# Remove the id column from dataframe, will only mess up
-# our calculations currently
-df.drop(['id'], axis=1, inplace=True)
+    # Remove the id column from dataframe, will only mess up
+    # our calculations currently
+    df.drop(['id'], axis=1, inplace=True)
 
-# Define Xs and ys (features and labels/class)
-X = np.array(df.drop(['class'], axis=1))  # X is everything but class
-y = np.array(df['class'])
+    # Define Xs and ys (features and labels/class)
+    X = np.array(df.drop(['class'], axis=1))  # X is everything but class
+    y = np.array(df['class'])
 
-# Test and train X and y
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # Test and train X and y
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Set up our classifier
-clf = neighbors.KNeighborsClassifier()
-# Then fit with trained data
-clf.fit(X_train, y_train)
-# And score on test data
-accuracy = clf.score(X_test, y_test)
+    # Set up our classifier
+    clf = neighbors.KNeighborsClassifier()
+    # Then fit with trained data
+    clf.fit(X_train, y_train)
+    # And score on test data
+    accuracy = clf.score(X_test, y_test)
 
-#print(accuracy)
+    #print(accuracy)
+    accuracies.append(accuracy)
+
+print(sum(accuracies)/len(accuracies))
 
 ##    # Make an example to predict with
 ##    example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
