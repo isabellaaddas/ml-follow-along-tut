f133fb5a27298c4dfdf7abababe8c61f80b7b474
isabellaaddas <isabellaaddas@gmail.com>
2026-08-04 11:35:45 -0400

    began tracking average accuracy by moving all code into for loop

--- knn-from-scratch.py
+++ knn-from-scratch.py
@@ -63,85 +63,93 @@ def k_nearest_neighbors(data, predict, k=3):
 
     return vote_result, confidence
 
-# Read in the data file and save it to our dataframe
-df = pd.read_csv('./k-nearest-files/breast-cancer-wisconsin.data')
+# Tally the accuracy of each full run of the data and
+# add it to a list to find the average accuracy
+# Note: 25 is a randomly picked count
+accuracies = []
+for i in range(25):
+    # Read in the data file and save it to our dataframe
+    df = pd.read_csv('./k-nearest-files/breast-cancer-wisconsin.data')
 
-# Replace all '?' values with outlier value
-df.replace('?', -99999, inplace=True)
+    # Replace all '?' values with outlier value
+    df.replace('?', -99999, inplace=True)
 
-# Drop the entire ID column from set
-df.drop(['id'], axis=1, inplace=True)
+    # Drop the entire ID column from set
+    df.drop(['id'], axis=1, inplace=True)
 
-# To correct for some values coming in as strings or
-# other value, copy data where every value is converted
-# to a float
-full_data = df.astype(float).values.tolist()
+    # To correct for some values coming in as strings or
+    # other value, copy data where every value is converted
+    # to a float
+    full_data = df.astype(float).values.tolist()
 
-# Use the random module to shuffle the data
-random.shuffle(full_data)
+    # Use the random module to shuffle the data
+    random.shuffle(full_data)
 
-# Define the size of our test and initialize both
-# the train and test sets as empty lists
-# (where the keys are the signifiers for benign or
-# malignant tumors)
-test_size = 0.2
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
+    # Define the size of our test and initialize both
+    # the train and test sets as empty lists
+    # (where the keys are the signifiers for benign or
+    # malignant tumors)
+    test_size = 0.2
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
 
-# Take the first 80% of full_data values into test_data
-# by using the test data size (test_size) as a *stop*
-# value
-train_data = full_data[:-int(test_size*len(full_data))]
+    # Take the first 80% of full_data values into test_data
+    # by using the test data size (test_size) as a *stop*
+    # value
+    train_data = full_data[:-int(test_size*len(full_data))]
 
-# Take the remaining 20% (defined by test_size) from
-# full_data values to put into test_data
-test_data = full_data[-int(test_size*len(full_data)):]
+    # Take the remaining 20% (defined by test_size) from
+    # full_data values to put into test_data
+    test_data = full_data[-int(test_size*len(full_data)):]
 
-# Populate train_set with train_data
-# i = the row of data in train_data
-# -1 = the last column in selected row where the tumor
-# signifier lies
-# append(i[:-1]) -> adds the row/list EXCEPT for the
-# attribute itself
-[train_set[i[-1]].append(i[:-1]) for i in train_data]
-# (logic copied for test_set)
-[test_set[i[-1]].append(i[:-1]) for i in test_data]
+    # Populate train_set with train_data
+    # i = the row of data in train_data
+    # -1 = the last column in selected row where the tumor
+    # signifier lies
+    # append(i[:-1]) -> adds the row/list EXCEPT for the
+    # attribute itself
+    [train_set[i[-1]].append(i[:-1]) for i in train_data]
+    # (logic copied for test_set)
+    [test_set[i[-1]].append(i[:-1]) for i in test_data]
 
-# Initialize counter variables
-correct = 0
-total = 0
+    # Initialize counter variables
+    correct = 0
+    total = 0
 
-# For each group in the test set (2 and 4)
-for group in test_set:
-    # Inner for-loop accesses each set of data
-    # in each group
-    for data in test_set[group]:
-        # Calculate the vote value using train_set,
-        # using the data in the test_set as the
-        # prediction value to find if the prediction
-        # was wrong or right
-        vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+    # For each group in the test set (2 and 4)
+    for group in test_set:
+        # Inner for-loop accesses each set of data
+        # in each group
+        for data in test_set[group]:
+            # Calculate the vote value using train_set,
+            # using the data in the test_set as the
+            # prediction value to find if the prediction
+            # was wrong or right
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
 
-        # If the group the data came from is equal to
-        # the vote (row of data that is classified as
-        # '2' was correctly identified as a member
-        # of the '2' class), correct counter increases
-        if group == vote:
-            correct += 1
-        else:
-            # Print the confidence (second return item
-            # from k-nearest-neighbors) for each
-            # incorrect prediction
-            print(confidence)
+            # If the group the data came from is equal to
+            # the vote (row of data that is classified as
+            # '2' was correctly identified as a member
+            # of the '2' class), correct counter increases
+            if group == vote:
+                correct += 1
+            else:
+                # Print the confidence (second return item
+                # from k-nearest-neighbors) for each
+                # incorrect prediction
+                print(confidence)
 
-        # Regardless of the result, our total counter
-        # increases on each pass
-        total += 1
+            # Regardless of the result, our total counter
+            # increases on each pass
+            total += 1
 
-# Print the accuracy of our results, which is simply
-# the ratio of correct results to total results
-# (how accurate/correct the predictions were)
-print('Accuracy:', correct/total)
+    # Print the accuracy of our results, which is simply
+    # the ratio of correct results to total results
+    # (how accurate/correct the predictions were)
+    print('Accuracy:', correct/total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies)/len(accuracies))
 
 # *** THE FOLLOWING WAS MERELY A DEMONSTRATION ***
 
