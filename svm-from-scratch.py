eec150867711b1f5ef39613eb5fbb3cc5b474ad0
isabellaaddas <isabellaaddas@gmail.com>
2026-09-03 22:34:49 -0400

    started initializing dictionary of data for dating

--- svm-from-scratch.py
+++ svm-from-scratch.py
@@ -8,3 +8,10 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
 style.use('ggplot')
+
+# Initialize dictionary for data where the keys are
+# classes
+data_dict = {-1:np.array([[1,7],
+                          [2,8],
+                          [3,8],]),
+             1:np.array([[1,7],])}
